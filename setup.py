9497b9026f90c1a9ffa1df92bb67fde5c57f8534
SHINYA HARADA <shinyaharada@furodrive.local>
2017-06-12 21:06:11 +0900

    init

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='install_composer',
+      version='0.1',
+      description='install composer',
+      author='furodrive',
+      license='MIT',
+      packages=['install_composer'],
+      zip_safe=False)
